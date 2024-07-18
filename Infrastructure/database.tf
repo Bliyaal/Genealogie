b577d5b1939d3559312c2c56620351a426eca101
Michel Sylvestre - SYLM <sylm@premiertech.com>
2024-07-18 13:03:57 -0400

    use capacity ref

--- Infrastructure/database.tf
+++ Infrastructure/database.tf
@@ -81,7 +81,7 @@ resource "kubernetes_persistent_volume_claim_v1" "postgres" {
 
     resources {
       requests = {
-        storage = "2Gi"
+        storage = kubernetes_persistent_volume_v1.postgres[each.key].spec[0].capacity[0].storage
       }
     }
 
